9091de0d215cab5d6d3260e62df29a4b2fbc100f
zhoukekestar <zhoukekestar@163.com>
2015-07-21 09:58:56 +0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## Simple Log System based on Nodejs & MongoDB.
+## 基于Nodejs和MongoDB的简单日志系统
--- app.js
+++ app.js
@@ -7,9 +7,13 @@ var cookieParser = require('cookie-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// index
 app.get("/", function(req, res){
 	res.sendfile("index.html");
 });
+
+// Get log
 app.get('/MongoDB/logs/:col', function(req, res){
 
 	var opt = req.query["options"];
@@ -18,17 +22,21 @@ app.get('/MongoDB/logs/:col', function(req, res){
 	else {
 		opt = eval("(" + opt + ")")
 	}
-	
+
 	mongo.select(req.param('col'), opt, function(err, docs){
 		// Jsonp request
 		res.jsonp(docs);
 	});
 });
+
+// Get log by id
 app.get('/MongoDB/logs/:col/:id', function(req, res){
 	mongo.selectByID(req.param('col'), req.param('id'), function(err, docs){
 		res.jsonp(docs);
 	});
 });
+
+// Add log
 app.post('/MongoDB/logs/:col', function(req, res){
 	try{
 		var doc = JSON.parse(req.body["doc"]);
@@ -39,12 +47,16 @@ app.post('/MongoDB/logs/:col', function(req, res){
 		res.send(e);
 	}
 });
+
+// Modify log by id
 app.put('/MongoDB/logs/:col/:id', function(req, res){
 	var doc = JSON.parse(req.body["doc"]);
 	mongo.update(req.param('col'), req.param('id'), doc, function(err, result) {
 		res.jsonp(result);
 	});
 });
+
+// Delete log by id
 app.delete('/MongoDB/logs/:col/:id', function(req, res){
 	mongo.delete(req.param('col'), req.param('id'), function(err, result){
 		res.jsonp(result);
